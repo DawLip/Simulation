cdb79714a8ce99e32fe6c0ce38de2075cec6378d
Qler-gra <karasinski.kacper@outlook.com>
2023-03-20 15:31:03 +0100

    Restructurisation 0.4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from GUI.GUI import GUI
-from model.y import y
+# from model.y import y
 
 if __name__ == "__main__":
     GUI()
--- model/constructors/cell.py
+++ model/constructors/cell.py
@@ -1,6 +1,12 @@
-def cellConstractor(x,y,energy):
+import sys,os,random
+sys.path.insert(1,os.path.abspath('.'))
+
+from data import data
+
+def cellConstractor(energy=100,x=random.randint(0,data["x"]),y=random.randint(0,data["y"])):
+    data["id"]+=1
     return {
-        "id":id,
+        "id":data["id"],
         "type":'cell',
         "x":x,
         "y":y,
