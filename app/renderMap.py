0f75deb69f7a3ed3aaacc4c9a21442018f8ea39c
Qler2123 <karasinski.kacper@outlook.com>
2023-03-25 11:55:36 +0100

    update renderMap.py change function to rendering all Entities by 1 for loop

--- app/renderMap.py
+++ app/renderMap.py
@@ -1,25 +1,18 @@
 import pygame
 
 from data import data, GUI
-# from model.food import Food
-# from model.cell import Cell
 from model.entity import Entity
 
-def renderMap():
-    for entity in Entity.all:
-        GUI['sprites'].append((
-            GUI['textures'][f'{entity.__class__.__name__.lower()}'],
-            (entity.x*GUI['texturesSize'], entity.y*GUI['texturesSize'])
-        ))
-        
-    # for cell in Cell.all:
-    #     GUI['sprites'].append((
-    #         GUI['textures']['cell'],
-    #         (cell.x*GUI['texturesSize'], cell.y*GUI['texturesSize'])
-    #     ))
 
-    # for food in Food.all:
-    #     GUI['sprites'].append((
-    #         GUI['textures']['food'],
-    #         (food.x*GUI['texturesSize'], food.y*GUI['texturesSize'])
-    #     ))
+def renderMap():
+    GUI["sprites"] = []
+    for entity in Entity.all:
+        # TODO add solution for Body
+        if entity.__class__.__name__ == "Body":
+            continue
+        GUI["sprites"].append(
+            (
+                GUI["textures"][f"{entity.__class__.__name__.lower()}"],
+                (entity.x * GUI["texturesSize"], entity.y * GUI["texturesSize"]),
+            )
+        )
--- model/initializeData.py
+++ model/initializeData.py
@@ -3,11 +3,13 @@ from random import randrange
 from data import data, GUI
 from .food import Food
 from .cell import Cell
-    
+
+
 def initializeData():
-    for _ in range(data['initialCellNumber']):
+    for _ in range(data["initialCellNumber"]):
         # TODO default values
-        Cell(randrange(data['simWidth']-1),randrange(data['simHeight']-1),30,45)
-    
-    for _ in range(data['initialFoodNumber']):
-        Food(randrange(data['simWidth']-1),randrange(data['simHeight']-1))
+        Cell(randrange(data["simWidth"] - 1), randrange(data["simHeight"] - 1), 10, 50)
+
+        # TODO default values
+    for _ in range(data["initialFoodNumber"]):
+        Food(randrange(data["simWidth"] - 1), randrange(data["simHeight"] - 1), 50)
