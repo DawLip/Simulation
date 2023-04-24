4d4e61661f353aaae01ac615a685100da731cd26
Qler-gra <karasinski.kacper@outlook.com>
2023-04-24 17:37:58 +0200

    change GUI button text to img

--- components/WindowTab.py
+++ components/WindowTab.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 
 class WindowTab(tk.Button):
-    def __init__(self, parent, column=0, row=0,  **args):
+    def __init__(self, parent, text, column=0, row=0,  **args):
+        if text == "AllEntitiesInfo" or text == "EntityInfo": self.img = tk.PhotoImage(file = "./resources/GUI/GUItmp1.png"),
+        elif text == "Simulation": self.img = tk.PhotoImage(file = "./resources/GUI/GUItmp2.png"),
+        else: self.img = tk.PhotoImage(file = "./resources/GUI/GUItmp3.png"),
         super().__init__(
-            parent, 
-            bg='#4B4B52', fg='#D8D8E6', 
-            activebackground='#4B4B52', activeforeground='#D8D8E6',
+            parent,
+            image=self.img,
+            borderwidth=0,
+            highlightthickness=0,
             **args
         )
-
+        print(text)
         self.grid(column=column, row=row, sticky='w')
